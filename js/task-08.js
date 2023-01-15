2d90cbdf33e615e0c98f6b91e54362630ef3fdf7
Oleksandr Tytyk <aleks.tit3@gmail.com>
2023-01-15 20:56:02 +0200

    add task 8, 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,20 @@
-// Задание 8
-// Напиши скрипт управления формой логина.
+const formEl = document.querySelector(".login-form");
 
-// <form class="login-form">
-//   <label>
-//     Email
-//     <input type="email" name="email" />
-//   </label>
-//   <label>
-//     Password
-//     <input type="password" name="password" />
-//   </label>
-//   <button type="submit">Login</button>
-// </form>
+formEl.addEventListener("submit", handleSubmit);
 
-// Обработка отправки формы form.login-form должна быть по событию submit.
-// При отправке формы страница не должна перезагружаться.
-// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
-// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены!");
+  } else {
+      console.log(`Email: ${email.value}, Password: ${password.value}`);
+      event.currentTarget.reset();
+  }
+  
+}
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const nameSpanColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener('click', bodyChangeColor)
+
+function bodyChangeColor(event) {
+  const randomColor = getRandomHexColor();
+
+  bodyColor.style.backgroundColor = randomColor;
+  nameSpanColor.textContent = randomColor;
+}
